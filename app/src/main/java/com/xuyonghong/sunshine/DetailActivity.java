d87bba86812c1fc17299d0813b3a33ffba47fed4
Yonhom <953410459@qq.com>
2017-02-08 14:13:21 +0800

    use toolbar instead of the default actionbar provided by default

--- app/src/main/java/com/xuyonghong/sunshine/DetailActivity.java
+++ app/src/main/java/com/xuyonghong/sunshine/DetailActivity.java
@@ -3,6 +3,7 @@ package com.xuyonghong.sunshine;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -14,6 +15,12 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        // use our own version of toolbar as action bar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         if (savedInstanceState == null) {
             // Create the detail fragment and add it to the activity
             // using a fragment transaction
@@ -27,7 +34,7 @@ public class DetailActivity extends AppCompatActivity {
                     .add(R.id.weather_detail_container, dFragment) // the
                     .commit();
         }
-        getSupportActionBar().setElevation(0);
+//        getSupportActionBar().setElevation(0);
 
     }
 
--- app/src/main/java/com/xuyonghong/sunshine/MainActivity.java
+++ app/src/main/java/com/xuyonghong/sunshine/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -39,6 +40,10 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
 
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
         if (findViewById(R.id.weather_detail_container) != null) { // the app is in two-panel mode
             // The detail container view will be present only in the large-screen layouts
             // (res/layout-sw600dp). If this view is present, then the activity should be
@@ -54,7 +59,7 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
             }
         } else {
             mTwoPane = false;
-            getSupportActionBar().setElevation(0); // remove the shadow under action bar on phone
+//            getSupportActionBar().setElevation(0); // remove the shadow under action bar on phone
         }
 
         // initialize the SyncAdapter
